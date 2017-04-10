e70faa079ef94e924d2e09ce92d8e26808deb3ab
daemon <1490802761@qq.com>
2017-04-10 22:03:09 +0800

    android c端

---
+++ ShengDian/app/src/main/java/com/dae/ecupl/shengdian/models/BannerModel.java
@@ -0,0 +1,17 @@
+package com.dae.ecupl.shengdian.models;
+
+/**
+ * Created by ASUS on 2017/4/7.
+ */
+
+import java.util.List;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:15/6/21 下午11:16
+ * 描述:
+ */
+public class BannerModel {
+    public List<String> imgs;
+    public List<String> tips;
+}
