233336198de6f9ea71873a4974e15877fbee32af
daemon <1490802761@qq.com>
2017-08-02 20:10:09 +0800

    移除后端部分

--- server_shengdian/routers/book.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ASUS on 2017/3/16.
- */
-var Book = require("../controllers").book;
-
-module.exports = function(server) {
-    server.get({path: "/api/book/grade"}, Book.grade);
-};
--- server_shengdian/test/testforJSON.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ASUS on 2017/2/15.
- */
-var querystring = require("querystring");
-var str = '{ "name": 123, "age": 42}';
-console.log(str);
-var obj = JSON.parse(str);
-console.log(obj.name);
